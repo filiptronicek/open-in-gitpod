c0818ac0a92ce8fb1cd127ed374619b9b1f3c4a4
Filip Troníček <filip.tronicek@seznam.cz>
2021-08-13 19:01:34 +0200

    Stuff works, lol

--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,59 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as cp from "child_process";
+import { URL } from "url";
 
-const execShell = (cmd: string) =>
-    new Promise<string>((resolve, reject) => {
-        cp.exec(cmd, (err, out) => {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(out);
-        });
+const execShell = (cmd: string, path: string) =>
+  new Promise<string>((resolve, reject) => {
+    cp.exec(cmd, { cwd: path }, (err, out) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(out);
     });
+  });
 
-export function activate (context: vscode.ExtensionContext) {
+const allowedDomains = ["github.com", "gitlab.com"];
+
+export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "vs-code-gitpod.open",
     () => {
       vscode.window.showInformationMessage("Opening... gaming");
 
-	// get the git origin
-	execShell(`cd ${process.env.cwd}; dir`).then(origin => {
-		console.log(origin);
-	}).catch(err => {
-		vscode.window.showErrorMessage("Error: " + err);
-	});
+      if (!vscode.workspace) {
+        return vscode.window.showErrorMessage(
+          "Please open a project folder first"
+        );
+      }
+
+      const folderPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
+      /*
+        .split(":")[1];
+*/
+      console.log(folderPath);
+
+      // get the git origin
+      execShell(`git config --get remote.origin.url`, folderPath || "./")
+        .then((origin) => {
+          const urlObject = new URL(origin);
+          const domain = urlObject.hostname;
+
+          // If the domain isn't allowed, tell the user that he can't open the project in GitPod
+          if (allowedDomains.includes(domain) === false) {
+            vscode.window.showErrorMessage(
+              `The project is not hosted on a GitLab or GitHub server and cannot be opened with GitPod.`
+            );
+            return;
+          } else {
+            // If the domain is allowed, open the project in GitPod (in the user's default browser)
+            vscode.env.openExternal(vscode.Uri.parse(`https://gitpod.io/#${origin}`));
+
+          }
+        })
+        .catch((err) => {
+          vscode.window.showErrorMessage("Error: " + err);
+        });
       //vscode.env.openExternal(vscode.Uri.parse("https://gitpod.io/"));
     }
   );
